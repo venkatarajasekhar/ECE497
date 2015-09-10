853f8e9a0f44b934d414ca9bafeb0ee165c6d0e9
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-09-10 20:43:54 +0530

    qabstracteventdispatcher.h

--- qt-everywhere-opensource-src-4.6.2/src/corelib/kernel/qabstracteventdispatcher.h
+++ qt-everywhere-opensource-src-4.6.2/src/corelib/kernel/qabstracteventdispatcher.h
@@ -44,12 +44,61 @@
 
 #include <QtCore/qobject.h>
 #include <QtCore/qeventloop.h>
+#include <vector>
 
 QT_BEGIN_HEADER
 
 QT_BEGIN_NAMESPACE
 
 QT_MODULE(Core)
+// we allow for 2^24 = 8^8 = 16777216 simultaneously running timers
+enum { NumberOfBuckets = 8, FirstBucketSize = 8 };
+
+static const int BucketSize[NumberOfBuckets] =
+    { 8, 64, 512, 4096, 32768, 262144, 2097152, 16777216 - 2396744 };
+static const int BucketOffset[NumberOfBuckets] =
+    { 0,  8,  72,  584,  4680,  37448,  299592,  2396744 };
+
+static int FirstBucket[FirstBucketSize] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+static QBasicAtomicPointer<int> timerIds[NumberOfBuckets] =
+    { Q_BASIC_ATOMIC_INITIALIZER(FirstBucket),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0) };
+
+#if defined(OS_LINUX_C++Programming)
+
+namespace QT_BEGIN_HEADER{
+
+namespace QT_BEGIN_NAMESPACE{
+
+namespace QT_MODULE(Core){
+// we allow for 2^24 = 8^8 = 16777216 simultaneously running timers
+typedef enum class { NumberOfBuckets = 8, FirstBucketSize = 8 };
+
+static const int BucketSize[NumberOfBuckets] =
+    { 8, 64, 512, 4096, 32768, 262144, 2097152, 16777216 - 2396744 };
+    vector <static const int> v_eventbucketsize(BucketSize);
+    
+static const int BucketOffset[NumberOfBuckets] =
+    { 0,  8,  72,  584,  4680,  37448,  299592,  2396744 };
+   vector <static const int> v_BucketEventOffset(BucketOffset);
+static int FirstBucket[FirstBucketSize] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+static QBasicAtomicPointer<int> timerIds[NumberOfBuckets] =
+    { Q_BASIC_ATOMIC_INITIALIZER(FirstBucket),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0),
+      Q_BASIC_ATOMIC_INITIALIZER(0) };
+vector <static QBasicAtomicPointer<int>> v_EvtBucketTimerID(timerIds);      
+      #endif
 
 class QAbstractEventDispatcherPrivate;
 class QSocketNotifier;
@@ -103,5 +152,8 @@ protected:
 QT_END_NAMESPACE
 
 QT_END_HEADER
-
+#if defined(OS_LINUX_C++Programming)
+} //QT_END_NAMESPACE
+} //QT_END_HEADER
+#endif
 #endif // QABSTRACTEVENTDISPATCHER_H
